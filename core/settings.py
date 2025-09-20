e1eb36d737899a43a6b988323f28f557e0ce528d
JoaoManoelFontes <manoel.joao@escolar.ifrn.edu.br>
2025-09-20 14:43:17 -0300

    feat: add postgres container to project

--- core/settings.py
+++ core/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+        "NAME": os.environ.get("POSTGRES_DB", "falaai_db"),
+        "USER": os.environ.get("POSTGRES_USER", "falaai"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "falaai"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
