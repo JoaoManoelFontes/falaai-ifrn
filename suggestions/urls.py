c0eb92ecfaee5b952dcd51fa3e6153bf1a24e056
Bruno Souza <brunosouzaff10@gmail.com>
2025-10-28 15:48:21 -0300

    feat: add create suggestion page

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views.suggestions import index
+from .views.suggestions import create_suggestion, index
 
 urlpatterns = [
     path("", index, name="index"),
+    path("suggestions/create/", create_suggestion, name="create_suggestion"),
 ]
--- suggestions/views/suggestions.py
+++ suggestions/views/suggestions.py
@@ -1,8 +1,29 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
+from suggestions.forms import SuggestionForm
 
 
 @login_required(login_url="auth")
 def index(request):
     """Página inicial das sugestões."""
     return render(request, "index.html")
+
+
+def create_suggestion(request):
+    """Página para criar uma nova sugestão."""
+
+    if request.method == "POST":
+        form = SuggestionForm(request.POST)
+        if form.is_valid():
+            suggestion = form.save(commit=False)
+            suggestion.customer = request.user.customer
+            suggestion.save()
+            messages.success(request, "Sugestão criada com sucesso!")
+            return redirect("index")
+
+        messages.error(request, "Por favor, corrija os erros abaixo.")
+
+    form = SuggestionForm()
+    return render(request, "suggestion_form.html", {"form": form})
