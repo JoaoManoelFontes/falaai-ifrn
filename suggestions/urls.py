12bb64837959f0e87c1f0e112a0162f8358b4f3d
JoaoManoelFontes <jmanufontesc@gmail.com>
2025-11-17 10:55:45 -0300

    feat: add profile view and template

--- suggestions/urls.py
+++ suggestions/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views.suggestions import create_suggestion, index
+from .views.suggestions import create_suggestion, index, profile
 
 urlpatterns = [
     path("", index, name="index"),
     path("suggestions/create/", create_suggestion, name="create_suggestion"),
+    path("profile/", profile, name="profile"),
 ]
